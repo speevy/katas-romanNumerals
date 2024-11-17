fd0c78a8c5db07393020a6736bc31525abf99117
ivan.lorca <speevy@speevy.net>
2024-11-17 10:41:48 +0100

    Initial commit

---
+++ src/main/java/net/speevy/katas/roman_numerals/RomanNumberFormat.java
@@ -0,0 +1,22 @@
+package net.speevy.katas.roman_numerals;
+
+import java.text.FieldPosition;
+import java.text.NumberFormat;
+import java.text.ParsePosition;
+
+public class RomanNumberFormat extends NumberFormat {
+    @Override
+    public StringBuffer format(double number, StringBuffer toAppendTo, FieldPosition pos) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public StringBuffer format(long number, StringBuffer toAppendTo, FieldPosition pos) {
+        return null;
+    }
+
+    @Override
+    public Number parse(String source, ParsePosition parsePosition) {
+        throw new UnsupportedOperationException();
+    }
+}
